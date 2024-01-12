25b19b64c79dd437d2bea6e8c614bf25324abeea
assemSD99 <assem.oujda@gmail.com>
2024-01-12 01:02:10 +0100

    l'ajout de fonction Modifier et faire son test

--- GestionProduit/src/main/java/ma/assemNihal/ProduitService.java
+++ GestionProduit/src/main/java/ma/assemNihal/ProduitService.java
@@ -16,7 +16,7 @@ public class ProduitService {
 		boolean verifnom = false;
 
 		for (Produit exProduit : produits) {
-			if (exProduit.getId()==(produit.getId())) {
+			if (exProduit.getId() == (produit.getId())) {
 				verifid = true;
 				break;
 			}
@@ -39,19 +39,40 @@ public class ProduitService {
 		}
 	}
 
-	
-
 	public List<Produit> Selecttous() {
 		return new ArrayList<>(produits);
 	}
 
 	public Produit SelectParId(Long id) {
 		for (Produit produit : produits) {
-			if (produit.getId()==id) {
+			if (produit.getId() == id) {
 				return produit;
 			}
 		}
 		return null;
+	}
+
+	public void Modifier(Produit produit) {
+		boolean produitEx = false;
+		int index = -1;
+
+		for (int i = 0; i < produits.size(); i++) {
+			if (produits.get(i).getId() == produit.getId()) {
+				produitEx = true;
+				index = i;
+				break;
+			}
+		}
+
+		if (produitEx) {
+			if (produit.getPrix() >= 0 && produit.getQuantite() >= 0) {
+				produits.set(index, produit);
+			} else {
+				throw new IllegalArgumentException("Le prix et la quantité doivent être positifs.");
+			}
+		} else {
+			throw new IllegalStateException("Le produit nexist pas.");
+		}
 	}
 
 }
